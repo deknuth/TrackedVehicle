320eb7ad3c280c7d87aff32ee5e853e8733ea9de
deknuth <lovenix@live.cn>
2017-09-24 22:26:14 +0800

    20170924

--- PCB/brushless/V1.2/src/main.c
+++ PCB/brushless/V1.2/src/main.c
@@ -35,21 +35,46 @@ enum MOS{
 #define I1_ON   EIMSK=1<<INT1;
 #define I1_OFF  EIMSK&=~(1<<INT1);
 
+#define CAPT_T_FALL	{ TCCR1B=1<<ICNC1|1<<CS11;TCNT1=0; ICR1=0;}
+#define CAPT_T_RIS	{ TCCR1B=1<<ICNC1|1<<ICES1|1<<CS11; TCNT1=0; }
+
 volatile unsigned int VelInte = 0;  // velocity integral
 volatile unsigned int LastValue = 0;
-volatile unsigned char stall = 0;
+volatile unsigned char stall = 1;
 volatile unsigned int speed = 0;
 unsigned char dir = 0;
-typedef struct pd{
-    unsigned char SetSpeed;	//  Desired Value
-    double Proportion;		//  Proportional Const
-    double Integral;		//  Integral Const
-    double Derivative;		//  Derivative Const
-    int LastError;			// Error[-1]
-    int PrevError;			// Error[-2]
-}PID;
+volatile unsigned char CaptStat = 1;
+int value = 0;
 
-PID *sptr;
+const unsigned int porp[256] = {
+	0,0,0,0,0,1,2,3,4,5,
+	6,7,8,9,10,11,12,13,14,15,
+	16,17,18,19,20,21,22,23,24,25,
+	26,27,28,29,30,31,32,33,34,35,
+	36,37,38,39,40,41,42,43,44,45,
+	46,47,48,49,50,51,52,53,54,55,
+	56,57,58,59,60,61,62,63,64,65,
+	66,67,68,69,70,71,72,73,74,75,
+	76,77,78,79,80,81,82,83,84,85,
+	86,87,88,89,90,91,92,93,94,95,
+	96,97,98,99,100,101,102,103,104,105,
+	106,107,108,109,110,111,112,113,114,115,
+	116,117,118,119,120,121,122,123,124,125,
+	126,127,128,129,130,131,132,133,134,135,
+	136,137,138,139,140,141,142,143,144,145,
+	146,147,148,149,150,151,152,153,154,155,
+	156,157,158,159,160,161,162,163,164,165,
+	166,167,168,169,170,171,172,173,174,175,
+	176,177,178,179,180,181,182,183,184,185,
+	186,187,188,189,190,191,192,193,194,195,
+	196,197,198,199,200,201,202,203,204,205,
+	206,207,208,209,210,211,212,213,214,215,
+	216,217,218,219,220,221,222,223,224,225,
+	226,227,228,229,230,231,232,233,234,235,
+	236,237,238,239,240,241,242,243,244,245,
+	246,247,248,249,250,251
+};
+
 static void commut(unsigned char phase);        //  commutation
 
 void PortInit(void)
@@ -67,29 +92,11 @@ void PortInit(void)
     PINC = 0x00;
 }
 
-double Kp = 0.7;
-double Ki = 0.4;		// 1.9
-double Kd = 0.004;
-
-void IncPIDInit(PID *sptr)
+void T1Init(void)
 {
-    sptr->LastError = 0; 		// Error[-1]
-    sptr->PrevError = 0; 		// Error[-2]
-    sptr->Proportion = 0.1; 	// Kp Proportional Const
-    sptr->Integral = 0.01;		// KiIntegral Const
-    sptr->Derivative = 0.01;	// Kd Derivative Const
-    sptr->SetSpeed = 80;		// setting
-}
-
-int IncPIDCalc(int NextSpeed,PID *sptr)
-{
-    int iError;
-    int value;
-    iError = sptr->SetSpeed - NextSpeed;	// add cacle
-    value = Kp * iError - Ki * sptr->LastError + Kd * sptr->PrevError;
-    sptr->PrevError = sptr->LastError;
-    sptr->LastError = iError;
-    return(value);
+	TCCR1B = 1<<ICNC1 | 1<<ICES1 | 1<<CS11;			// rising trigger
+	TIMSK1 = 1<<ICIE1;
+	TCNT1 = 0;
 }
 
 void I1Init(void)
@@ -107,53 +114,47 @@ void T0Init(void)
 {
     TCCR0A = 1<<COM0A1 | 1<<COM0B1 | 1<<WGM00;		// 8bit phase
     TCCR0B = 1<<CS00;			// 0 divide
-    OCR0A = OCR0B = 0x40;
+    OCR0A = OCR0B = 0x00;
 }
 
 void T2Init(void)
 {
     TCCR2A = 1<<COM2A1 | 1<<WGM20;
     TCCR2B = 1<<CS20;
-    OCR2A = 0x40;
+    OCR2A = 0x00;
 }
 
-volatile unsigned char len = 0;
-volatile unsigned char flag = 0;
-volatile int sp = 0;
-unsigned int value = 0;
-unsigned char sBuf[3] = {0};
+volatile unsigned char update = 0;
 unsigned char times = 0;
-ISR(TIMER1_OVF_vect)
-{
-    D2_BLINK;
-//    sBuf[1] = speed&0xFF;
-//    sBuf[0] = (speed>>8);
-//   SendStr(sBuf,2);
-    sp += IncPIDCalc(speed,sptr);
-    if(sp > 255)
-        sp = 255;
-    if(sp < 0)
-        sp = 0;
-    OCR0A = OCR0B = OCR2A = (255-sp);
-    speed = 0;
-    if(times++ > 8)
-    {
-        if(LastValue == VelInte)
-            stall = 1;
-        else
-        {
-            LastValue = VelInte;
-            stall = 0;
-        }
-        times = 0;
-    }
-    if(VelInte > 60000)
-        VelInte = 0;
-}
 
-ISR(INT1_vect)
+ISR(TIMER1_CAPT_vect)
 {
-    speed++;
+	if(CaptStat)
+	{
+		CaptStat = 0;
+		CAPT_T_FALL;
+	}
+	else			// falling tigger
+	{
+		CaptStat=1;
+		CAPT_T_RIS;
+		D2_BLINK;
+		speed = ICR1L;
+		if(times++ > 2)
+		{
+			if(LastValue == VelInte)
+				stall = 1;
+			else
+			{
+				LastValue = VelInte;
+				stall = 0;
+			}
+			times = 0;
+		}
+		if(VelInte > 60000)
+			VelInte = 0;
+		update = 1;
+	}
 }
 
 ISR(PCINT1_vect)
@@ -191,26 +192,6 @@ int StartFun(unsigned char SetSpeed)
             OCR0A = OCR0B = OCR2A = temp;
     }
 over:
-    /*
-    if(speed > SetSpeed)
-    {
-        for(i=temp; i>SetSpeed; i--)
-        {
-            OCR1A = OCR1B = OCR2A = i;
-            _delay_ms(3);
-        }
-    }
-    else
-    {
-        for(i=temp; i<SetSpeed; i++)
-        {
-            OCR1A = OCR1B = OCR2A = i;
-            _delay_ms(1);
-        }
-    }*/
-    speed = 0;
-    T1_ON;
-    I1_ON;
     return 1;
 }
 
@@ -272,28 +253,47 @@ void commut(unsigned char phase)        //  commutation
 
 int main(void)
 {
-    sptr = (PID *)malloc(sizeof(PID));
-    IncPIDInit(sptr);
+	cli();
     PortInit();
-    PCInit();
     LED_BLINK;
     _delay_ms(500);
     LED_BLINK;
-    sei();
+	PCInit();
     T0Init();
     T2Init();
-    I1Init();
-    while(!StartFun(0x80))
-        _delay_ms(500);
+	T1Init();
+	sei();
     while(1)
     {
-        if(stall)
-        {
-            while(!StartFun(0x80))
-                _delay_ms(500);
-        }
+		if(update)
+		{
+			update = 0;
+			value = speed-1500;
+			if(value >= 0)
+			{
+				dir = 0;
+				value >>= 1;
+				if(value > 255)
+					value = 255;
+				value = porp[value];
+			}
+			else
+			{
+				value = abs(value);
+				value >>= 1;
+				if(value > 255)
+					value = 255;
+				value = porp[value];
+				dir = 1;
+			}
+			if(stall && value>1)
+			{
+				while(!StartFun(0x80));
+				OCR0A = OCR0B = OCR2A = value;
+			}
+			OCR0A = OCR0B = OCR2A = value;
+		}
     }
-    free(sptr);
 }
 
 
